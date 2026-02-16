134f5232698172175bd67a6e1abca042e656024e
Adam <adam.belshaw@hicomply.com>
2026-02-16 15:08:15 +0000

    add zxbasicus to the pipeline to create a more optimised build

--- make.bat
+++ make.bat
@@ -7,6 +7,18 @@ REM append data statements to BASIC and build
 type main.zxb udgs.zxb routines.zxb > build.zxb
 .\tools\zmakebas -l -a @begin -o calirun.tap build.zxb
 
+REM convert the .tap file to .bas that can be optimised with zxbasicus
+.\tools\zxbasicus\zxbasicus -a -i calirun.tap
+del calirun_0.info
+del calirun_0.lst
+ren calirun_0.bas
+REM create optimiuse .bas file
+.\tools\zxbasicus\zxbasicus -t --alloptim -i calirun_0.bas -o calirun.zxb
+del calirun_0.bas
+REM create .tap file from .bas file
+del calirun.tap
+.\tools\zxbasicus\zxbasicus -s --line 10 --progname calirun -i calirun.zxb -o calirun.tap
+
 REM tidy up
 del udgs.zxb
 del routines.zxb
